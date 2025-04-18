d5c449df79a4408219eb096b21309c56f3c3224a
Stella Sun <stellasun0332@gmail.com>
2025-04-18 13:18:54 -0500

    implemented wave 02

--- app/routes/planet_route.py
+++ app/routes/planet_route.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response
 from app.models.planet import planets
 
 # create a blueprint
@@ -15,3 +15,29 @@ def get_all_planets():
       diameter = planet.diameter
     ))
   return result_list
+
+@planets_bp.get("/<planet_id>")
+def get_a_planet(planet_id):
+  planet = validate_planet(planet_id)
+  planet_dic = dict(
+          id = planet.id,
+          name = planet.name,
+          description = planet.description,
+          diameter = planet.diameter
+  )
+  return planet_dic
+
+#helper function to validate if the planet id can be converted to an int and exits in the planets
+def validate_planet(planet_id):
+  try:
+    planet_id = int(planet_id)
+  except:
+    invalid = {"message": f"Planet id {planet_id} is invalid."}
+    abort(make_response(invalid, 400))
+  
+  for planet in planets:
+    if planet.id == planet_id:
+      return planet
+  
+  not_found = {"message": f"Planet id {planet_id} not found."}
+  abort(make_response(not_found, 404))
