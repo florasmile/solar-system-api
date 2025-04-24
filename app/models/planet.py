189e2c27294e4cea0e47512490f43ab1c899dede
dehuihu <dehuihu@gmail.com>
2025-04-24 11:54:30 -0700

    Setup database and adds Planet model

--- app/models/planet.py
+++ app/models/planet.py
@@ -1,14 +1,26 @@
-class Planet:
-  def __init__(self, id, name, description, diameter):
-    self.id = id
-    self.name = name
-    self.description = description
-    self.diameter = diameter
-    # assume diameter is in kilomiles
-planets = [
-    Planet(1, "Mercury", "The smallest planet in our solar system.", 3.0),
-    Planet(2, "Venus", "The hottest planet with a thick atmosphere.", 7.5),
-    Planet(3, "Earth", "Our home planet, the only one known to support life.", 7.9),
-    Planet(4, "Mars", "The red planet with the tallest volcano.", 4.2)
-]
+from sqlalchemy.orm import Mapped, mapped_column
+from ..db import db
+
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str]
+    description: Mapped[str]
+    diameter: Mapped[float]
+
+
+
+# class Planet:
+#   def __init__(self, id, name, description, diameter):
+#     self.id = id
+#     self.name = name
+#     self.description = description
+#     self.diameter = diameter
+#     # assume diameter is in kilomiles
+# planets = [
+#     Planet(1, "Mercury", "The smallest planet in our solar system.", 3.0),
+#     Planet(2, "Venus", "The hottest planet with a thick atmosphere.", 7.5),
+#     Planet(3, "Earth", "Our home planet, the only one known to support life.", 7.9),
+#     Planet(4, "Mars", "The red planet with the tallest volcano.", 4.2)
+# ]
+
 
--- app/routes/planet_route.py
+++ app/routes/planet_route.py
@@ -1,43 +1,43 @@
 from flask import Blueprint, abort, make_response
-from app.models.planet import planets
+# from app.models.planet import planets
 
 # create a blueprint
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
-@planets_bp.get("/")
-def get_all_planets():
-  result_list = []
-  for planet in planets:
-    result_list.append(dict(
-      id = planet.id,
-      name = planet.name,
-      description = planet.description,
-      diameter = planet.diameter
-    ))
-  return result_list
+# @planets_bp.get("/")
+# def get_all_planets():
+#   result_list = []
+#   for planet in planets:
+#     result_list.append(dict(
+#       id = planet.id,
+#       name = planet.name,
+#       description = planet.description,
+#       diameter = planet.diameter
+#     ))
+#   return result_list
 
-@planets_bp.get("/<planet_id>")
-def get_a_planet(planet_id):
-  planet = validate_planet(planet_id)
-  planet_dic = dict(
-          id = planet.id,
-          name = planet.name,
-          description = planet.description,
-          diameter = planet.diameter
-  )
-  return planet_dic
+# @planets_bp.get("/<planet_id>")
+# def get_a_planet(planet_id):
+#   planet = validate_planet(planet_id)
+#   planet_dic = dict(
+#           id = planet.id,
+#           name = planet.name,
+#           description = planet.description,
+#           diameter = planet.diameter
+#   )
+#   return planet_dic
 
-#helper function to validate if the planet id can be converted to an int and exits in the planets
-def validate_planet(planet_id):
-  try:
-    planet_id = int(planet_id)
-  except:
-    invalid = {"message": f"Planet id {planet_id} is invalid."}
-    abort(make_response(invalid, 400))
+# #helper function to validate if the planet id can be converted to an int and exits in the planets
+# def validate_planet(planet_id):
+#   try:
+#     planet_id = int(planet_id)
+#   except:
+#     invalid = {"message": f"Planet id {planet_id} is invalid."}
+#     abort(make_response(invalid, 400))
   
-  for planet in planets:
-    if planet.id == planet_id:
-      return planet
+#   for planet in planets:
+#     if planet.id == planet_id:
+#       return planet
   
-  not_found = {"message": f"Planet id {planet_id} not found."}
-  abort(make_response(not_found, 404))
+#   not_found = {"message": f"Planet id {planet_id} not found."}
+#   abort(make_response(not_found, 404))
